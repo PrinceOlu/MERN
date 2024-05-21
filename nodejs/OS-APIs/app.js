9d280a51553fe952644d71d80047257f9b2d52cd
Prince <ojugbeleolusegun@gmail.com>
2024-05-20 19:14:49 -0700

    Node completed

---
+++ nodejs/OS-APIs/app.js
@@ -0,0 +1,20 @@
+const os = require("os");
+// console.log(os);
+
+// 1) Accessing the system architecture
+// console.log(`this is my computer details`, os.arch());
+
+// 2) Accessing the CPU
+// console.log(`this is my computer details`, os.cpus());
+
+// 3) Accessing the free memory
+// console.log(`this is my computer details`, os.endianness());
+
+// 4) Accessing the homedir
+// console.log(`this is my computer details`, os.homedir());
+
+// 5) Accessing the hosname
+// console.log(`this is my computer details`, os.hostname());
+
+// 6) Accessing the networkinterface
+console.log(`this is my computer details`, os.networkInterfaces());
---
+++ nodejs/nodejs-global-object/app.js
@@ -0,0 +1,33 @@
+// Accessing the global objects
+// Note: the console.log() is a global method
+
+// console.log(global);
+
+global.myGlobal = "Hello from the global object";
+// console.log(global.myGlobal);
+
+// To check if our global varible is truely global
+// console.log("myGlobal" in global);
+
+// to check the directory name
+// console.log(__dirname);
+
+// to check the filename
+// console.log(__filename);
+
+// using setInterval() and clearInterval()
+
+// let count = 0;
+// const intervalId = setInterval(() => {
+//   console.log("Hello world");
+//   count++;
+//   if (count === 5) {
+//     clearInterval(intervalId);
+//   }
+// }, 1000);
+
+// using setTimeout()
+
+setTimeout(() => {
+  console.log("Hello world");
+}, 1000);
