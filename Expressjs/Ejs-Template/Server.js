ec9a3e3b0c19173c165c0f482a186034cea17cac
Prince <ojugbeleolusegun@gmail.com>
2024-06-04 03:36:55 -0700

    ejs template and static pages

---
+++ Expressjs/Ejs-Template/Server.js
@@ -0,0 +1,44 @@
+const express = require("express");
+const app = express();
+const path = require("path");
+const PORT = 3000;
+
+// serve the static files/folders
+// using a middleware
+app.use(express.static(path.join(__dirname, "public")));
+
+// setup the ejs view engine
+app.set("view engine", "ejs");
+
+// render home route
+app.get("/", (req, res) => {
+  res.render("home");
+});
+// render about route
+app.get("/about", (req, res) => {
+  res.render("about");
+});
+// render contact route
+app.get("/contact", (req, res) => {
+  res.render("contact");
+});
+// render gallery route
+app.get("/gallery", (req, res) => {
+  res.render("gallery");
+});
+// render user data route
+app.get("/users", (req, res) => {
+  // dummy db
+  const userData = {
+    username: "Prince",
+    age: 20,
+    isPremiumUser: true,
+    email: "prince@gmail.com",
+  };
+  res.render("userData.ejs", userData);
+});
+
+// start the server
+app.listen(PORT, () => {
+  console.log("server is running...");
+});
