2b6d2c4592e69ba5664b1c00b2b392df2615bbfc
Prince <ojugbeleolusegun@gmail.com>
2024-06-25 15:55:45 -0700

    cookies completed

--- Authentication/cookies/cookie/server.js
+++ Authentication/cookies/cookie/server.js
@@ -1,116 +1,83 @@
 const express = require("express");
 const cookieParser = require("cookie-parser");
-const mongoose = require("mongoose");
-const bcrypt = require("bcryptjs");
-
 const app = express();
 const PORT = 3000;
 
 // Middlewares
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
-// Set the view engine
+// lets set the viw engine
 app.set("view engine", "ejs");
 
-// Connect to mongoose
-const url =
-  "mongodb+srv://ojugbeleolusegun:wNjrUe0sC0ERZpbB@mern-cluster0.7hdkpgd.mongodb.net/students-db";
-
-const connectDB = async () => {
-  try {
-    await mongoose.connect(url, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-    });
-    console.log("Database connected...");
-  } catch (error) {
-    console.log("Error in connection", error);
-  }
-};
-connectDB();
-
-// Create user schema
-const userSchema = new mongoose.Schema({
-  username: String,
-  password: String,
-  role: {
-    type: String,
-    default: "User",
+// simulated user database
+const users = [
+  {
+    username: "Prince",
+    password: "123",
+    role: "admin",
   },
-});
-// Create model from the schema
-const UserApp = mongoose.model("UserApp", userSchema);
+  {
+    username: "PrinMoyoce",
+    password: "456",
+    role: "user",
+  },
+];
 
-// Create the home route
+// lets create the home route
 app.get("/", (req, res) => {
   res.render("home");
 });
 
-// Create the register-form route
-app.get("/register", (req, res) => {
-  res.render("register");
-});
-
-// Create the register route logic
-app.post("/register", async (req, res) => {
-  const { username, password } = req.body;
-  const hashedPassword = await bcrypt.hash(password, 10);
-  try {
-    await UserApp.create({
-      username: username,
-      password: hashedPassword,
-    });
-    res.redirect("/login");
-  } catch (error) {
-    console.log("Error creating user", error);
-    res.status(500).send("Error creating user");
-  }
-});
-
-// Create the login-form route
+// lets create the login-form route
 app.get("/login", (req, res) => {
   res.render("login");
 });
 
-// Create the login route logic
-app.post("/login", async (req, res) => {
-  const { username, password } = req.body;
-  const userFound = await UserApp.findOne({ username: username });
-
-  if (userFound && (await bcrypt.compare(password, userFound.password))) {
-    res.cookie("loginUser", JSON.stringify(userFound), {
-      maxAge: 3 * 24 * 60 * 60 * 1000, // 3 days expiration
-      httpOnly: true, // to prevent XSS attacks
-      secure: false, // set to true in production stage
-      sameSite: "strict",
-    });
+// lets create the login route logic
+app.post("/login", (req, res) => {
+  // 1) find the user login details
+  //   console.log(req.body);
+  const userFound = users.find((user) => {
+    const { username, password } = req.body;
+    return user.username === username && user.password === password;
+  });
+  //   console.log(userFound);
+  // 2) create some cookies for the user
+  res.cookie("loginUser", JSON.stringify(userFound), {
+    maxAge: 3 * 24 * 60 * 1000, //3days expiration
+    httpOnly: true, // to prevemt xxs attacks
+    secure: false, // set to true in production stage
+    sameSite: "strict",
+  });
+  // 3) Render the user dashboard
+  if (userFound) {
     res.redirect("/dashboard");
-  } else {
-    res.status(401).send("Invalid username or password");
   }
 });
 
-// Create the dashboard route
+// lets create the dashboard route
 app.get("/dashboard", (req, res) => {
+  // grab the user from the cookie from login
   const loginUser = req.cookies.loginUser
     ? JSON.parse(req.cookies.loginUser)
     : null;
   const username = loginUser ? loginUser.username : null;
-
+  //   console.log(req.cookies);
   if (username) {
     res.render("dashboard", { username });
   } else {
+    // redirect to login if user does not exist
     res.redirect("/login");
   }
 });
-
-// Create the logout route
+// lets create the logout route
 app.get("/logout", (req, res) => {
   res.clearCookie("loginUser");
+  //   redirect to login page
   res.redirect("/login");
 });
 
 // Start the server
 app.listen(PORT, () => {
-  console.log("Server started on port " + PORT);
+  console.log("server started..");
 });
