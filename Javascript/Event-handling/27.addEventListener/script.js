40ecba83283b845692b65db9e00fa9a0a2231556
Prince <ojugbeleolusegun@gmail.com>
2024-09-09 04:08:38 -0700

    updated...

--- Javascript/Event-handling/27.addEventListener/script.js
+++ Javascript/Event-handling/27.addEventListener/script.js
@@ -1,10 +1,11 @@
 // ========== eventistener and chaining ====
 
 // ========== basic method =========
-// const clickButton = document.getElementById("clickButton");
-// clickButton.addEventListener("click", function () {
-//   console.log("click event is called");
-// });
+const clickButton = document.getElementById("clickButton");
+clickButton.addEventListener("click", function () {
+  // console.log("click event is called");
+  console.log(this); // does not bind to arrow function
+});
 
 // =========chaining====================
 // ============= advance method 1======
@@ -23,7 +24,7 @@ document.getElementById("clickButton").addEventListener("click", ClickButton);
 // doubleClickButton.addEventListener("dblclick", function doubleClickButton() {
 //   console.log("double click event called");
 // });
-//   ===double click method 1=====
+//   ===double click method 2=====
 // document
 //   .getElementById("doubleClickButton")
 //   .addEventListener("dblclick", function () {
@@ -43,3 +44,25 @@ function hoverDiv() {
   console.log("on mouse over");
 }
 document.getElementById("hoverDiv").addEventListener("mouseover", hoverDiv);
+
+//   ======== mouse out advance method 2======
+function hoverDiv() {
+  console.log("on mouse out");
+}
+document.getElementById("hoverDiv").addEventListener("mouseout", hoverDiv);
+
+// =================== keyboard events =========
+
+const formInput = document
+  .getElementById("formInput")
+  .addEventListener("keydown", function () {
+    console.log("keypress event is called");
+  });
+
+// ================= form event ==============
+const myForm = document
+  .getElementById("myForm")
+  .addEventListener("submit", function (e) {
+    e.preventDefault();
+    console.log("form submitted...");
+  });
