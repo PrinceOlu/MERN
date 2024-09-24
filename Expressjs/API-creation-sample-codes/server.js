9c7251887a936c4d56f34140999c83aa7b06a2f9
Prince Olusegun Ojugbele <ojugbeleolusegun@gmail.com>
2024-09-24 03:55:57 -0700

    Resolved merge conflicts

--- Expressjs/API-creation-sample-codes/server.js
+++ Expressjs/API-creation-sample-codes/server.js
@@ -1,65 +1,68 @@
-// initialise expres
 const express = require("express");
-// create an intance of express
 const app = express();
-// create the port
+
 const PORT = 3000;
-
-// lets add a middle-ware to catch the post from the new book added
+// to bre able to allow the client 
+// to send data to the server
+// we use a middleware
 app.use(express.json());
-
-// lets create a dummy db
+// dummy data
 const books = [
-  { id: "1", title: "Prince Book", author: "Prince" },
-  { id: "2", title: "Moyo Book", author: "Moyo" },
-  { id: "3", title: "Ade Book", author: "Ade" },
+  {id:"1",name:"Prince"},
+  {id:"2",name:"Ade"},
+  {id:"3",name:"Mofe"},
 ];
 
 // lets create the home route
-app.get("/", (req, res) => {
+app.get("/",(req,res)=>{
   res.json({
-    status: "Succesful",
-    message: "Welcome to my home page",
-  });
-});
-// lets create all the book route
-app.get("/books", (req, res) => {
+    status:"successfull",
+    message:"Welcome to our home page"
+  })
+})
+
+// get  all books
+app.get("/books/",(req,res)=>{
   res.json({
-    status: "Succesful",
-    message: "All books",
-    data: books,
-  });
-});
-// lets create a single book route
+    status:"successful",
+    message:"All books found",
+    data:books,
+  })
+})
+
+// Get a single book by ID
 app.get("/books/:id", (req, res) => {
   const id = req.params.id;
-  const bookFound = books.find((book) => book.id === id);
-  if (bookFound) {
+  
+  // Find the book by matching the ID
+  const foundBook = books.find((book) => book.id === id);
+
+  if (foundBook) {
     res.json({
-      status: "Succesful",
-      message: "All books",
-      data: bookFound,
+      status: "successful",
+      message: "book found",
+      data: foundBook
     });
   } else {
     res.json({
-      status: "Succesful",
-      message: "All books",
-      data: "Book not found",
+      status: "Unsuccessful",
+      message: "book not found"
     });
   }
 });
-// lets add new book route
-app.post("/books", (req, res) => {
-  // console.log(req.body);
-  const addBook = req.body;
-  books.push(addBook);
+// Add a new book
+app.post("/books/", (req, res) => {
+  const newBook = req.body;
+  // Add the new book to the books array
+  books.push(newBook);
   res.json({
-    status: "Succesful",
-    message: "book added successfully",
-    data: addBook,
+    status: "Successful",
+    message: "Book added successfully",
+    data: newBook, // Return the newly added book
   });
 });
-// start the server
-app.listen(PORT, () => {
-  console.log("Server started...");
-});
+
+app.listen(PORT,()=>{
+  console.log(`Server running on port ${PORT}`);
+  
+})
